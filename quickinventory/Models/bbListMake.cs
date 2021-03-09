08d6737a8fecbf0be0120ecd72fb32bb7166dd96
DEV16952 <DEV16952@LLBJ-DJ50UOJ>
2021-03-08 19:53:11 -0600

    testing gitt

--- quickinventory/Models/bbListMake.cs
+++ quickinventory/Models/bbListMake.cs
@@ -1,13 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using SimpleInventory.Common;
+
 //
 namespace SimpleInventory.Models
 {
-   public class bbListVendor : bbBaseList
+    class bbListMake : bbBaseList
     {
         [ForeignKey("Tenant")]
         public long? Tenant_ID { get; set; }
         public bbTenant Tenant { get; set; }
-        public Address  Address { get; set; }
+        public Address Address { get; set; }
     }
 }
